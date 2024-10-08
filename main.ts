a3494b71ba2ffef370f66b06e8c23f2ab9ad020a
Elliott-Roach <elliott.roach@mths.ca>
2024-10-08 10:13:09 -0400

    editd info

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Elliott Roach
+ * Created on: Sep 202
+ * This program says hello World
 */
 
 basic.showString('Hello, World!')
