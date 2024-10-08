9527b2e8d7260c151078ea437c956326d9fae391
Elliott-Roach <elliott.roach@mths.ca>
2024-10-08 11:16:24 -0400

    coded traffic light

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Elliott Roach
- * Created on: Sep 202
- * This program says hello World
+ * Created on: Sep 2024
+ * This program is a traffic light
 */
 
-basic.showString('Hello, World!')
+let neopixleStrip: neopixel.Strip = null
+//clean
+basic.clearScreen()
+neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixleStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    
+//green
+    basic.clearScreen()
+    neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.show()
+    pause(1000)
+
+//yellow
+    neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.show()
+    pause(1000)
+
+//red
+    neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    neopixleStrip.show()
+    pause(1000)
+
+//clean
+    neopixleStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    neopixleStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixleStrip.show()
+    basic.showIcon(IconNames.Happy)
+})
