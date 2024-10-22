c81d882b9d429732302a7ced8c7c205f818ad6d2
Elliott-Roach <elliott.roach@mths.ca>
2024-10-22 15:24:41 +0000

    started trafik light

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,23 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Elliott Roach
+Created on: oct 2024
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
+display.clear()
+sleep(1000)
 
-display.scroll("Hello, World!")
+neopixel[0] = (0, 0, 0)
+neopixel[1] = (0, 0, 0)
+neopixel[2] = (0, 0, 0)
+neopixel[3] = (0, 0, 0)
+
+display.show(Image.HAPPY)
+
+if button_a.is_pressed():
+    neopixel[0] = (255, 0, 0)
+    neopixel[1] = (0, 0, 0)
+    neopixel[2] = (0, 0, 0)
+    neopixel[3] = (0, 0, 0)
